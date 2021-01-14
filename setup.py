3efca4d8c601e4c6fe35115b5f92e10b18a20442
Elias Grünewald <gruenewald@campus.tu-berlin.de>
2021-01-14 21:12:24 +0100

    Set MIT License for PyPi package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tilt",
-    version="0.0.4",
+    version="0.0.5",
     author="Elias Grünewald",
     author_email="gruenewald@tu-berlin.de",
     description="A python language binding for the Transparency Information Language and Toolkit (TILT)",
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
